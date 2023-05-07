5a550343b2dc56decc78db9086d4bc307a464509
见山云木 <42599908+xfc-exclave@users.noreply.github.com>
2023-05-07 15:21:46 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,9 +96,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>小陈同学(•̤̀ᵕ•̤́๑)ᵒᵏᵎᵎᵎᵎ</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          你专属的聊天问答助手~~
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
